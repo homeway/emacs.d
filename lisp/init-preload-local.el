e6eae20ce10de446657c622250a6017f5626dfbe
homeway <homeway.xue@gmail.com>
2014-10-15 18:47:31 +0800

    试一下mac下的字体设置

--- lisp/init-preload-local.el
+++ lisp/init-preload-local.el
@@ -1,3 +1,9 @@
+;; -----------------------------------------------------------------------------
+;; setting font for mac system
+;; 下面的设置可针对mac的X窗口版本设置字体，但考虑终端更好用，所以就免了这个设置吧
+;;(set-fontset-font "fontset-default" 'han '("STSong" . "unicode-bmp"))
+
+;; -----------------------------------------------------------------------------
 ;; 避免在文件相同目录下产生结尾为~的备份文件，但保留备份文件的机制
 (setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
       backup-by-copying      t  ; Don't de-link hard links
@@ -6,15 +12,19 @@
       kept-new-versions      20 ; how many of the newest versions to keep
       kept-old-versions      5) ; and how many of the old
 
+;; -----------------------------------------------------------------------------
 ;; 去掉菜单显示（mac下无法使用）
 (menu-bar-mode -1)
 
+;; -----------------------------------------------------------------------------
 ;; 颜色模板
 (load-theme 'tango-dark' t)
 
+;; -----------------------------------------------------------------------------
 ;; 默认打开大纲缩进
 (setq org-startup-indented t)
 
+;; -----------------------------------------------------------------------------
 ;; 在org文件中支持语法高亮
 (setq org-src-fontify-natively t)
 
