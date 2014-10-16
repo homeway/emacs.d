79f0ab2382fe8b1e2c9f172b0d74735765b5e6f4
homeway <homeway.xue@gmail.com>
2014-10-16 08:32:21 +0800

    新增从org导出pdf的article模板
    独立标题页
    段落缩进两个字符

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -3,4 +3,86 @@
 (require 'projectile)
 (projectile-global-mode)
 
+;; ------------------------------
+;; 使用xelatex从org生成PDF的模板
+(setq org-latex-to-pdf-process
+'("xelatex -interaction nonstopmode %f"
+"xelatex -interaction nonstopmode %f"))
+
+;; code执行免应答（Eval code without confirm）
+(setq org-confirm-babel-evaluate nil)
+;; Auctex
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq-default TeX-master nil)
+(defun org-mode-article-modes ()
+  (reftex-mode t)
+  (and (buffer-file-name)
+       (file-exists-p (buffer-file-name))
+       (reftex-parse-all)))
+(add-hook 'org-mode-hook
+          (lambda ()
+            (if (member "REFTEX" org-todo-keywords-1)
+                (org-mode-article-modes))))
+(unless (boundp 'org-export-latex-classes)
+  (setq org-export-latex-classes nil))
+;; 从org文件导出pdf的模板
+;; 需要在org文件中增加 #+LATEX_CLASS: pdf
+(add-to-list 'org-export-latex-classes
+             '("pdf"
+               "\\documentclass[10pt,a4paper,titlepage]{article}
+\\usepackage{graphicx}
+\\usepackage{xcolor}
+\\usepackage{xeCJK}
+\\usepackage{lmodern}
+\\usepackage{verbatim}
+\\usepackage{fixltx2e}
+\\usepackage{longtable}
+\\usepackage{float}
+\\usepackage{tikz}
+\\usepackage{wrapfig}
+\\usepackage{soul}
+\\usepackage{textcomp}
+\\usepackage{listings}
+\\usepackage{geometry}
+\\usepackage{algorithm}
+\\usepackage{algorithmic}
+\\usepackage{marvosym}
+\\usepackage{wasysym}
+\\usepackage{latexsym}
+\\usepackage{natbib}
+\\usepackage{fancyhdr}
+\\usepackage[xetex,colorlinks=true,CJKbookmarks=true,linkcolor=blue,urlcolor=blue,menucolor=blue]{hyperref}
+\\usepackage{fontspec,xunicode,xltxtra}
+\\setmainfont[BoldFont=Courier]{Courier}
+\\setsansfont[BoldFont=Courier]{Courier}
+\\setmonofont{Courier New}
+\\newcommand\\fontnamemono{Courier New}%等宽字体
+\\newfontinstance\\MONO{\\fontnamemono}
+\\newcommand{\\mono}[1]{{\\MONO #1}}
+\\setCJKmainfont[Scale=0.9]{STSong}%中文字体
+\\setCJKmonofont[Scale=0.9]{STSong}
+\\hypersetup{unicode=true}
+\\geometry{a4paper, textwidth=6.5in, textheight=10in,marginparsep=7pt, marginparwidth=.6in}
+\\punctstyle{kaiming}
+\\title{}
+\\fancyfoot[C]{\\bfseries\\thepage}
+\\chead{\\MakeUppercase\\sectionmark}
+\\pagestyle{fancy}
+\\tolerance=1000
+\%%%% 段落首行缩进两个字 %%%%
+\\makeatletter
+\\let\\@afterindentfalse\\@afterindenttrue
+\\@afterindenttrue
+\\makeatother
+\\setlength{\\parindent}{2em}  %中文缩进两个汉字位
+[NO-DEFAULT-PACKAGES]
+[NO-PACKAGES]"
+("\\section{%s}" . "\\section*{%s}")
+("\\subsection{%s}" . "\\subsection*{%s}")
+("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+("\\paragraph{%s}" . "\\paragraph*{%s}")
+("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+(put 'upcase-region 'disabled nil)
+
 (provide 'init-local)
